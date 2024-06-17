b1659bdd5e810941cef58c209fdc590a9ff697f7
clem-amrd <clem.amiard@gmail.com>
2024-06-17 08:25:16 +0200

    update indices

--- src/app/components/useFetchRandomMusic.js
+++ src/app/components/useFetchRandomMusic.js
@@ -1,20 +1,36 @@
 import { useState, useEffect, useRef } from 'react';
 
-export function useFetchRandomMusic(APIChan, triggerFetch, initializedMusic) {
-    const [musicData, setMusicData] = useState({ musicId: null, artist: '', title: ''});
+export function useFetchRandomMusic(API, triggerFetch, initializedMusic) {
+    const [musicData, setMusicData] = useState({ musicId: null, artist: '', title: '', tabTitles: []});
     useEffect(() => {
         if (!initializedMusic.current) {
         const fetchRandomMusic = async () => {
             try {
-                const string = createRandomString(1);
-                const response = await fetch(`https://api.musixmatch.com/ws/1.1/track.search?apikey=${APIChan}&f_has_lyrics&page_size=100&f_has_rich_sync=1&f_lyrics_language=fr&s_track_rating=desc&q=${string}`);
-                const data = await response.json();
+                let lyricLanguage = JSON.parse(localStorage.getItem("Langues") || []);
+                let genre = JSON.parse(localStorage.getItem("Genres") || []);
+                console.log(genre);
+                console.log(lyricLanguage);
 
+                const randomGenre = Math.floor(Math.random() * genre.length);
+                const randomlyricLanguage = Math.floor(Math.random() * lyricLanguage.length);
+                console.log(randomGenre);
+                console.log(randomlyricLanguage);
+                const string = createRandomString(1);
+                const url = `https://api.musixmatch.com/ws/1.1/track.search?apikey=${API}&f_has_lyrics&page_size=100&f_has_rich_sync=1&s_track_rating=desc&q=${string}`;
+                if(lyricLanguage.length > 0){
+                    url + `&f_music_genre_id=${genre[randomGenre]}`;
+                }
+                if(genre.length > 0){
+                    url + `&f_lyrics_language=${lyricLanguage[randomlyricLanguage]}`;
+                }
+                const response = await fetch(url);
+                const data = await response.json();
                 const tracks = data.message.body.track_list;
                 if (tracks.length > 0) {
                     const randomNumber = Math.floor(Math.random() * tracks.length);
                     const randomTrack = tracks[randomNumber].track;
-                    setMusicData({ musicId: randomTrack.track_id, artist: randomTrack.artist_name, title: randomTrack.track_name });
+                    let tab = [tracks[randomNumber].track.track_name, tracks[randomNumber+1].track.track_name, tracks[randomNumber+2].track.track_name, tracks[randomNumber+3].track.track_name]
+                    setMusicData({ musicId: randomTrack.track_id, artist: randomTrack.artist_name, title: randomTrack.track_name , tabTitles: tab});
                 } else {
                     console.log('No tracks found');
                 }
@@ -25,7 +41,7 @@ export function useFetchRandomMusic(APIChan, triggerFetch, initializedMusic) {
         fetchRandomMusic();
         initializedMusic.current = true;
     }
-    }, [APIChan, triggerFetch]);
+    }, [API, triggerFetch]);
 
     return musicData;
 }
--- src/app/dashboard/game/page.tsx
+++ src/app/dashboard/game/page.tsx
@@ -10,32 +10,41 @@ export default function Page() {
     const APIChan2 = "fea0e303280abb2f95f0cec07585cc19";
     const APIChan3 = "4ac4dc543416ddc84a76fc91c72cd98e";
 
-    let ScoreTotal: number = 0;
-    let ScoreManche: number = 100;
+    const [ScoreTotal, setScoreTotal] = useState(0);
+    const [ScoreManche, setScoreManche] = useState(100);
+    const [nbManche, setNbManche] = useState(0);
+    const [inputVisible, setInputVisible] = useState(true);
+    const [choicesVisible, setChoicesVisible] = useState(false);
+    const [choices, setChoices] = useState([]);
     let initializedMusic = useRef(false);
     let initializedLyric = useRef(false);
 
-    const [triggerFetch, setTriggerFetch] = useState(false); // État pour déclencher une nouvelle fetch
+    const [triggerFetch, setTriggerFetch] = useState(false);
     const [timeLeft, setTimeLeft] = useState(90);
     const [showEntireLyric, setShowEntireLyric] = useState(false);
+    const [showArtist, setShowArtist] = useState(false);
     const [currentLyric, setCurrentLyric] = useState({ entireLyric: '', halfLyric: '' });
 
-    const { musicId, artist, title} = useFetchRandomMusic(APIChan3, triggerFetch, initializedMusic);
-    const { entireLyric, halfLyric } = useFetchLyrics(APIChan3, musicId, initializedLyric);
+    const { musicId, artist, title, tabTitles } = useFetchRandomMusic(APIChan2, triggerFetch, initializedMusic);
+    const { entireLyric, halfLyric } = useFetchLyrics(APIChan2, musicId, initializedLyric);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setTimeLeft(prevTime => {
-                if (prevTime <= 1) {
+                if (prevTime <= 0) {
                     clearInterval(interval);
+                    if (nbManche < 10) {
+                        generateNewMusic();
+                    } else {
+                        endGame();
+                    }
                     return 0;
                 }
                 return prevTime - 1;
             });
         }, 1000);
-
         return () => clearInterval(interval);
-    }, []);
+    }, [triggerFetch, nbManche]);
 
     useEffect(() => {
         if (entireLyric && halfLyric) {
@@ -49,72 +58,105 @@ export default function Page() {
         event.preventDefault();
         const input = document.getElementById('answer')?.value;
         const inputHTML = document.getElementById('answer');
-        console.log(title);
-        console.log(input);
-        if(inputHTML)
-        if (input === title) {
-            inputHTML.style.backgroundColor = '#69E688';
-            ScoreTotal += ScoreManche;
-            ScoreManche = 0;
-            initializedMusic.current = false;
-            initializedLyric.current = false;
-            generateNewMusic();
-        }else{
-            inputHTML.style.backgroundColor = '#E36032';
+        if (inputHTML) {
+            if (input === title) {
+                inputHTML.style.backgroundColor = '#69E688';
+                if (nbManche < 10) {
+                    generateNewMusic();
+                } else {
+                    endGame();
+                }
+            } else {
+                inputHTML.style.backgroundColor = '#E36032';
+            }
         }
     }
 
-    function revealeArtist() {
-        const pArtist = document.getElementById("rvArtist");
-        if (pArtist) pArtist.textContent = artist;
+    function revealArtist() {
+        setShowArtist(true);
+        setScoreManche(ScoreManche => ScoreManche - 20);
     }
 
     function addLyric() {
         setShowEntireLyric(true);
+        setScoreManche(ScoreManche => ScoreManche - 20);
+    }
 
+    function showChoices() {
+        setInputVisible(false);
+        setChoicesVisible(true);
+        setScoreManche(ScoreManche => ScoreManche - 30);
+        setChoices(tabTitles);
+    }
+
+    function handleChoiceClick(choice) {
+        if (title === choice) {
+            generateNewMusic();
+        }
     }
 
     function generateNewMusic() {
+        setInputVisible(true);
+        setChoicesVisible(false);
+        setChoices([]);
+        setScoreTotal(ScoreTotal => ScoreTotal + ScoreManche);
+        setScoreManche(100);
+        setNbManche(nbManche => nbManche + 1);
+
+        initializedMusic.current = false;
+        initializedLyric.current = false;
+
         setTriggerFetch(!triggerFetch);
         setTimeLeft(90);
         setShowEntireLyric(false);
+        setShowArtist(false);
         setCurrentLyric({ entireLyric: '', halfLyric: '' });
     }
 
+    function endGame() {
+        console.log("fin de partie, score = " + ScoreTotal);
+    }
+
     return (
-        <main>
-            <div>
-                <p>jeu</p>
-                <p>paroles</p>
+        <main className="flex flex-col items-center p-5">
+            <h1 className="text-3xl font-bold mb-4">Jeu de Paroles</h1>
+            <div className="grid grid-cols-2 gap-4">
                 <div>
-                    <p>Temps restant : {Math.floor(timeLeft / 60)}:{(timeLeft % 60).toString().padStart(2, '0')}</p>
-                    <div style={{
-                        width: '100%',
-                        height: '20px',
-                        backgroundColor: 'lightgray',
-                        borderRadius: '10px',
-                        overflow: 'hidden',
-                        marginTop: '10px'
-                    }}>
-                        <div style={{
-                            width: `${percentageLeft}%`,
-                            height: '100%',
-                            backgroundColor: 'green',
-                            transition: 'width 1s linear'
-                        }}></div>
+                    <div className="mb-5 text-center">
+                        <p id="lyric" className="text-lg overflow-y-auto h-96 w-auto p-6" dangerouslySetInnerHTML={{ __html: showEntireLyric ? currentLyric.entireLyric : currentLyric.halfLyric }}></p>
+                        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={addLyric}>Plus de paroles</button>
                     </div>
                 </div>
-                <br></br>
-                <p id="lyric" dangerouslySetInnerHTML={{ __html: showEntireLyric ? currentLyric.entireLyric : currentLyric.halfLyric }}></p>
-                <button onClick={addLyric}>Plus de paroles</button>
-                <form onSubmit={validateAnswer} method="post">
-                    <input type="text" id="answer" name="answer" placeholder="réponse" required minLength="1" size="15" />
-                    <input type="submit" value="valider" />
-                </form>
-            </div>
-            <div>
-                <button onClick={revealeArtist}>dévoiler l'artiste</button>
-                <div id="rvArtist"></div>
+                <div>
+                <div className="mb-5 text-center">
+                        <p className="text-xl">Temps restant : {Math.floor(timeLeft / 60)}:{(timeLeft % 60).toString().padStart(2, '0')}</p>
+                        <div className="w-full bg-gray-300 rounded-full h-4 mt-2">
+                            <div className="bg-green-500 h-4 rounded-full" style={{ width: `${percentageLeft}%` }}></div>
+                        </div>
+                    </div>
+                    {inputVisible && (
+                        <form onSubmit={validateAnswer} method="post" className="mb-5">
+                            <input type="text" id="answer" name="answer" placeholder="Réponse" required minLength="1" size="15" className="border border-gray-400 p-2 rounded w-64 mb-2" />
+                            <input type="submit" value="Valider" className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700 cursor-pointer" />
+                        </form>
+                    )}
+                    {choicesVisible && (
+                        <div id="rvBtnChoice" className="grid grid-cols-2 gap-4 mb-5">
+                            {choices.map((choice, index) => (
+                                <button key={index} className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-700" onClick={() => handleChoiceClick(choice)}>
+                                    {choice}
+                                </button>
+                            ))}
+                        </div>
+                    )}
+                    <div className="mb-5">
+                        <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700" onClick={revealArtist}>Dévoiler l'artiste</button>
+                        <div id="rvArtist" className="mt-2 text-lg" dangerouslySetInnerHTML={{ __html: showArtist ? artist : "" }}></div>
+                    </div>
+                    <div>
+                        <button className="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-700" onClick={showChoices}>Choix multiple</button>
+                    </div>
+                </div>
             </div>
         </main>
     );
